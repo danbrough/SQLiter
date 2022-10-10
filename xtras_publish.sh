516886d9977257c7dacaff64b4c612b562afbb78
Dan Brough <dan@danbrough.org>
2022-10-11 07:42:54 +1300

    update

--- xtras_publish.sh
+++ xtras_publish.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 cd `dirname "$0"`
 
-TASKS="publishAllPublicationsToMavenRepository"
+TASKS="publishAllPublicationsToSonaTypeRepository"
+[ -z "$SONATYPE_USER" ] && echo SONATYPE_USER not set && exit 1
+[ -z "$SONATYPE_PASSWORD" ] && echo SONATYPE_PASSWORD not set && exit 1
+[ -z "$SONATYPE_REPO_ID" ] && echo SONATYPE_REPO_ID not set && exit 1
+
+
+OPTS="-PRELEASE_REPOSITORY_URL=https://s01.oss.sonatype.org/service/local/staging/deployByRepositoryId/$SONATYPE_REPO_ID \
+ -PSONATYPE_NEXUS_USERNAME=$SONATYPE_USER -PSONATYPE_NEXUS_PASSWORD=$SONATYPE_PASSWORD"
 
 if [ "$(uname)" = "Darwin" ]; then
 	TASKS="$(cat mac_targets.txt)"
-fi 
+fi
 
-./gradlew  \
- -PRELEASE_REPOSITORY_URL=https://s01.oss.sonatype.org/service/local/staging/deployByRepositoryId/$SONATYPE_REPO_ID \
- -PSONATYPE_NEXUS_USERNAME=$SONATYPE_USER -PSONATYPE_NEXUS_PASSWORD=$SONATYPE_PASSWORD \
-  $TASKS
+./gradlew $OPTS $TASKS
 
 
